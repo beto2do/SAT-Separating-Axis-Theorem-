7d4f17acc9ba75368e7e6385de468f4ed9dc53d4
Humberto Segundo Rosas <humberto.segundo@diesvi.com>
2016-04-13 18:26:15 -0500

    I cleaned the code

--- src/mudmap.js
+++ src/mudmap.js
@@ -1,45 +1,9 @@
 function makeMudMap(config){
   var outside = {},
       draw = kendo.drawing,
-      geom = kendo.geometry,
-      Rect = kendo.geometry.Rect,
       Path = draw.Path;
 
-      //funcion duplicada
-  var getVectorBetweenTwoPoints = function(pointA, pointB){
-    var abscissa = pointB.getX() - pointA.getX();
-    var coordinate = pointB.getY() - pointA.getY();
-    return new geom.Point(abscissa, coordinate);
-  }
-
-  var areThereCollision = function(axis, vectorP, vectorA, vectorB){
-
-    var magnitudeProjection1 = projectionAB(vectorA, axis);
-    var magnitudeProjection2 = projectionAB(vectorB, axis);
-    var magnitudeProjection3 = projectionAB(vectorP, axis);
-    var response = true;
-
-    var gap = magnitudeProjection3 - magnitudeProjection1 - magnitudeProjection2;
-    if(gap > 0){
-      response = false;
-    }
-    return response;
-  }
-
-  var projectionAB = function(vectorA, vectorP){
-    var dotProduct = (vectorA.getX() * vectorP.getX()) + (vectorA.getY() * vectorP.getY());
-    var magnitudeVectorP = getModuloVector(vectorP);
-    var magnitudProjecttion = dotProduct / magnitudeVectorP;
-    return Math.abs(magnitudProjecttion);
-  }
-
-  var getModuloVector = function(vector){
-    var abscissaPow2 =  Math.pow(vector.getX(), 2);
-    var coordinatePow2 =  Math.pow(vector.getY(), 2);
-    return Math.sqrt(abscissaPow2 + coordinatePow2);
-  }
-
-  var getRect = function(rectangles){
+  var createRectangles2D = function(rectangles){
     var arrayRect = [];
     rectangles.forEach(function(item){
       var rectangle = makeRectangle(item.abscissa, item.coordinate,item.percent,item.amount);
@@ -48,54 +12,30 @@ function makeMudMap(config){
     return arrayRect;
   }
 
-  var areThereCollisionBetweenShares = function(rectA, rectB){
-    var centerPointA = rectA.getCenterPoint();
-    var centerPointB = rectB.getCenterPoint();
-
-    var vectorP = getVectorBetweenTwoPoints(centerPointA,centerPointB);
-
-    var vectorsA = rectA.getVectors();
-    var vectorsB = rectB.getVectors();
-    var axisA = rectA.getAxies();
-    var axisB = rectB.getAxies();
-    var response = true;
-    shareA:
-    for(w = 0; w < axisA.length; w++){
-      for(i = 0; i < vectorsA.length; i++){
-        for(j = 0; j < vectorsB.length; j++){
-          if(!areThereCollision(axisA[w],vectorP,vectorsA[i],vectorsB[j])){
-            response = false;
-            break shareA;
-          }
-        }
-      }
+  function getRandomColor() {
+    var letters = '0123456789ABCDEF'.split('');
+    var color = '#';
+    for (var i = 0; i < 6; i++ ) {
+        color += letters[Math.floor(Math.random() * 16)];
     }
-
-    return response;
+    return color;
   }
 
-  var areaCanvas = new Rect([0, 0], [config.canvaWith, config.canvaHeight]);
-  var layout = new draw.Layout(areaCanvas,{
-    alignContent:"start",
-    alignItems: "start",
-    justifyContent: "start"
-  });
-
-  var pathRects = getRect(config.rectangles);
+  var pathRects = createRectangles2D(config.rectangles);
   var rect1 = pathRects[0];
   var rect2 = pathRects[1];
 
-   var collision = areThereCollisionBetweenShares(rect1,rect2);
-  if(collision){
-    console.error("hay colision :(");
+   var overlap = rect1.hasOverlap(rect2);
+  if(overlap){
+    console.error("There is overlap :(");
   }else{
-    console.info("NO HAY colision :))))");
+    console.info("There isn't overlap :)");
   }
   var surface = draw.Surface.create($("#surface"));
 
   pathRects.forEach(function(rectangle){
-    var rect = Path.fromRect(rectangle.getRect());
-    rect.fill("orange");
+    var rect = Path.fromRect(rectangle.getKendoRect());
+    rect.fill(getRandomColor());
     surface.draw(rect);
   });
 
